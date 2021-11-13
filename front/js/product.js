28d949c652982b9ea5e05834941376bdd6b3edea
Anaïs Rosa <anaisrosa.lppgmail.com>
2021-11-13 22:53:36 +0100

    add to cart in progress

--- front/js/product.js
+++ front/js/product.js
@@ -25,7 +25,6 @@ const singleProductDisplay = async () => {
   //image à récupérer
   // console.log(singleProductData.imageUrl);
   // console.log(singleProductData.altTxt);
-
   document.getElementById("item__img").innerHTML +=
     "<img src =" +
     singleProductData.imageUrl +
@@ -39,7 +38,7 @@ const singleProductDisplay = async () => {
 
   //prix à récupérer
   // console.log(singleProductData.price);
-  document.getElementById("price").innerHTML += singleProductData.price;
+  document.getElementById("price").innerHTML += singleProductData.price + " ";
 
   //description à récupérer
   // console.log(singleProductData.description);
@@ -47,16 +46,71 @@ const singleProductDisplay = async () => {
     singleProductData.description;
 
   // Couleurs à récupérer
-  console.log(singleProductData.colors);
-  console.log(singleProductData.colors.length);
-
-
+  // console.log(singleProductData.colors);
+  // console.log(singleProductData.colors.length);
   let colorList = singleProductData.colors;
 
-colorList.forEach((color ,i ) => {
-  document.getElementById("colors").innerHTML += '<option value='+ singleProductData.colors[i]+ '>' + singleProductData.colors[i] + '</option>'
-     });
-  
+  colorList.forEach((color, i) => {
+    document.getElementById("colors").innerHTML +=
+      "<option value=" +
+      singleProductData.colors[i] +
+      ">" +
+      singleProductData.colors[i] +
+      "</option>";
+  });
 };
 
 singleProductDisplay();
+
+//Ajout d'un produit dans le panier
+let colorPicked = document.getElementById("colors");
+let amountOfItems = document.getElementById("quantity");
+const addToCartBtn = document.getElementById("addToCart");
+
+const addProductToCart =  () => {
+addToCartBtn.addEventListener("click", (e) => {
+    // alert("hello")
+    if (
+      amountOfItems.value > 1 &&
+      amountOfItems.value < 100 &&
+      amountOfItems.value !== 0
+    ) {
+      //Recupération des couleurs et quantités selectionnés
+      colorPicked = colorPicked.value;
+      amountOfItems = amountOfItems.value;
+
+      //création d'un objet par item mis dans le panier:
+    let product = { 
+    itemId : singleProductData._id,
+    itemPicture : singleProductData.imageUrl,
+    itemAltText : singleProductData.altTxt,
+    itemName : singleProductData.name,
+    itemPrice : singleProductData.price,
+    itemDescription : singleProductData.description,
+    itemColorPicked : colorPicked.value,
+    itemAmount : amountOfItems.value
+    }
+  // console.log(product);
+
+  //activation du LOCAL STORAGE
+  
+let itemSelected = JSON.parse(localStorage.getItem('item'));
+// console.log(itemSelected);
+
+//s'il y a dejà des des produits dans le panier 
+if(itemSelected) {
+  // itemSelected.push(product);
+  // localStorage.setItem("item", JSON.stringify(itemSelected));
+
+  //s'il n'y a pas de produits dans le panier
+} else {
+  itemSelected = [];
+  itemSelected.push(product);
+  localStorage.setItem("item", JSON.stringify(itemSelected));
+  console.log(itemSelected);
+}
+    }
+  });
+};
+
+addProductToCart();
